379a1269f38f146820b3dfd8a9b4cc2d273fe40a
Andrew Koltyakov <koltyakov@gmail.com>
2017-11-28 21:37:12 +0300

    Misc

--- tests/init.ts
+++ tests/init.ts
@@ -2,14 +2,14 @@ import * as path from 'path';
 import { AuthConfig } from '../src/index';
 
 const authConfig = new AuthConfig({
-    configPath: path.join(__dirname, '../config/private.json'),
-    defaultConfigPath: path.join(__dirname, '../config/default.json')
+  configPath: path.join(__dirname, '../config/private.json'),
+  defaultConfigPath: path.join(__dirname, '../config/default.json')
 });
 
 authConfig.getContext()
-    .then(context => {
-        console.log(JSON.stringify(context, null, 2));
-    })
-    .catch(error => {
-        console.log(error);
-    });
+  .then(context => {
+    console.log(JSON.stringify(context, null, 2));
+  })
+  .catch(error => {
+    console.log(error);
+  });
