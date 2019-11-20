55f55ac6790872c5d969f3c671a71cd6b30d5d9e
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2019-11-20 14:10:44 +0300

    Reverse to previous version of commander

--- src/cli.ts
+++ src/cli.ts
@@ -17,18 +17,18 @@ program
 program
   .command('init')
   .description('writes new file with node-sp-auth credentials into the file system')
-  .option('-p, --path [value]', 'relative path to file which will store your credentials, required')
-  .option('-e, --encrypt [true, false]', 'specify false if you don\'t need to encrypt password in the file, optional, default is true', true)
-  .option('-k, --masterkey [value]', 'optional key used to encrypt and decrypt your sensitive data (passwords), by default unique machine id is used', null)
+  .option('-p, --path [value]', 'relative path to file which will store your credentials (default: \'./config/private.json\')', './config/private.json')
+  .option('-e, --encrypt [true, false]', 'specify false if you don\'t need to encrypt password in the file (default: true)', true)
+  .option('-k, --masterkey [value]', 'optional key used to encrypt and decrypt passwords (default: unique machine id)', null)
   .action(initAction);
 
 program
   .command('read')
   .description('reads credentials from a private.json file')
-  .option('-p, --path [value]', 'relative path to file which will store your credentials, required')
-  .option('-e, --encrypt [true, false]', 'specify false if you don\'t need to encrypt password in the file, optional, default is true', true)
-  .option('-k, --masterkey [value]', 'optional key used to encrypt and decrypt your sensitive data (passwords), by default unique machine id is used', null)
-  .option('-f, --format', 'optional key used configure formatted output')
+  .option('-p, --path [value]', 'relative path to file which will store your credentials (default: \'./config/private.json\')', './config/private.json')
+  .option('-e, --encrypt [true, false]', 'specify false if you don\'t need to encrypt password in the file, optional (default: true)', true)
+  .option('-k, --masterkey [value]', 'optional key used to encrypt and decrypt passwords (default: unique machine id)', null)
+  .option('-f, --format', 'optional key used configure formatted output', false)
   .action(readAction);
 
 program.parse(process.argv);
